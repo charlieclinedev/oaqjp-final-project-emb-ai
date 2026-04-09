716d2f19db09e82c926b4f6a2c3bf33ba7a36297
charlieclinedev <charlie.cline@gmail.com>
2026-04-09 11:24:21 -0500

    added flask server

---
+++ server.py
@@ -0,0 +1,29 @@
+"""This module is for the server code for emotion detector"""
+from flask import Flask, request, render_template
+from EmotionDetection.emotion_detection import emotion_detector
+
+
+# initialize the flask app
+app = Flask('Emotion Detecotor')
+
+
+@app.route('/emotionDetector')
+def detect_emotion():
+    text_to_analyze = request.args.get('textToAnalyze')
+
+    response = emotion_detector(text_to_analyze)
+
+    output_string = f'For the given statement, the system response is ' \
+    f"'anger': {response['anger']}, 'disgust': {response['disgust']}, " \
+    f"'fear': {response['fear']}, 'joy': {response['joy']} and 'sadness': " \
+    f"{response['sadness']}. The dominant emotion is {response['dominant_emotion']}."
+
+    return output_string
+
+
+@app.route("/")
+def render_index_page():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)

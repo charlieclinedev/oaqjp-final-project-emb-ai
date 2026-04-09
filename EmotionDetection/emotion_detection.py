f826d3c17597276015e8095acacff2a5edc2f7b7
charlieclinedev <charlie.cline@gmail.com>
2026-04-09 10:31:33 -0500

    packaged project

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,24 +1,32 @@
 """Module providing functions for emotion detection."""
-
-'''
-URL: 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
-Headers: {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
-Input json: { "raw_document": { "text": text_to_analyze } }
-'''
-
 import requests
 import json
 
-
 def emotion_detector(text_to_analyze):
-    # URL of the sentiment analysis service
+    # URL of the emotion predictor service
     url = 'https://sn-watson-emotion.labs.skills.network/v1/' \
     'watson.runtime.nlp.v1/NlpService/EmotionPredict'
 
     # Create a dictionary with the text to be analyzed
     myobj = { "raw_document": { "text": text_to_analyze } }
+
     # Set the headers required for the API request
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+
     # Send a POST request to the API with the text and headers
     response = requests.post(url, json = myobj, headers=header)
+
     formatted_response = json.loads(response.text)
+    emotion_scores_json = formatted_response['emotionPredictions'][0]['emotion']
+
+    dominant_emotion = "anger"
+
+    for emotion in emotion_scores_json:
+        if emotion_scores_json[emotion] > emotion_scores_json[dominant_emotion]:
+            dominant_emotion = emotion
+
+    emotion_scores_json['dominant_emotion'] = dominant_emotion
+
+    return json.dumps(emotion_scores_json, indent=4)
+
+print(emotion_detector("I love this new technology"))
